588c31129f23268378090815bb80d56d680883db
Lukas <lukas-mueller@kabelmail.de>
2025-06-02 20:06:22 +0200

    Added a tip

--- src/main/java/com/cosmic/snakegamecraft/util/Tips.java
+++ src/main/java/com/cosmic/snakegamecraft/util/Tips.java
@@ -7,9 +7,11 @@ public class Tips {
     public static final List<String> TIPS = List.of(
             "🛠 You can change speed in the Settings.",
             "🍏 Modern Mode has bonus apples and difficulty scaling.",
-            "💡 Play Crazy Mode for AI enemies and weapons!",
+            " Play Crazy Mode for AI enemies and weapons!",
             "🔄 Your snake gets faster as it grows in Modern Mode.",
+            "💡 The Highscore scales with your speed multiplier.",
             "💾 Login to save highscores and custom settings."
+
     );
 
 }
